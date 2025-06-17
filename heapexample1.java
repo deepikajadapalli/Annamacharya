71f14c894cc7b7fb245078da5a5b38c10fcf4665
deepikajadapalli <168529161+deepikajadapalli@users.noreply.github.com>
2025-06-17 21:10:23 +0530

    Add files via upload

---
+++ heapexample1.java
@@ -0,0 +1,31 @@
+package Project;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class heapexample1 {
+
+	public static void main(String[] args) {
+		 
+		PriorityQueue<Integer>minHeap=new PriorityQueue<Integer>();
+		minHeap.add(20);
+		minHeap.add(5);
+		minHeap.add(15);
+		minHeap.add(10);
+		System.out.println("MinHeap output");
+		while(!minHeap.isEmpty()) {
+			System.out.println(minHeap.poll());
+		}
+		PriorityQueue<Integer> maxheap=new PriorityQueue<Integer>(Collections.reverseOrder());
+		maxheap.add(20);
+		maxheap.add(5);
+		maxheap.add(15);
+		maxheap.add(10);
+		System.out.println("MaxHeap output");
+		while(!maxheap.isEmpty()) {
+			System.out.println(maxheap.poll());
+
+	}
+
+}
+}
